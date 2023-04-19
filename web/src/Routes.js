5a96a27918c7bf89ffa910e23049e690c47f4b8f
rutvip <rp925@njit.edu>
2023-04-19 17:10:01 -0400

    Updated chapter 2 with minor updates from chapter 1

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/order */
 // In this file, all Page components from 'src/pages` are auto-imported. Nested
 // directories are supported, and should be uppercase. Each subdirectory will be
 // prepended onto the component name.
@@ -7,13 +8,25 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route } from '@redwoodjs/router'
+import { Route, Router, Set } from '@redwoodjs/router'
+
+import BlogLayout from './layouts/BlogLayout/BlogLayout'
+import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 
 const Routes = () => {
   return (
     <Router>
-      <Route path="/about" page={AboutPage} name="about" />
-      <Route path="/" page={HomePage} name="home" />
+      <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
+        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
+        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
+        <Route path="/posts" page={PostPostsPage} name="posts" />
+      </Set>
+      <Set wrap={BlogLayout}>
+        <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
+        <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/" page={HomePage} name="home" />
+      </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
---
+++ web/src/pages/ArticlePage/ArticlePage.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/order */
+import ArticleCell from 'src/components/ArticleCell'
+import { MetaTags } from '@redwoodjs/web'
+
+const ArticlePage = ({ id }) => {
+  return (
+    <>
+      <MetaTags title="Article" description="Article page" />
+
+      <ArticleCell id={id} />
+    </>
+  )
+}
+
+export default ArticlePage
